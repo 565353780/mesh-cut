78454365f6104ecc1cb8156826355008540bd74a
chLi <565353780@qq.com>
2025-06-17 21:38:33 +0800

    refine format

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,8 +1,8 @@
 cd ..
 git clone git@github.com:565353780/diff-curvature.git
 
-cd diff-curvature
-./dev_setup.sh
-
 pip install -U numpy open3d scipy scikit-learn matplotlib \
   tqdm numba
+
+cd diff-curvature
+./dev_setup.sh
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 cd ..
 git clone https://github.com/565353780/diff-curvature.git
 
-cd diff-curvature
-./setup.sh
-
 pip install -U numpy open3d scipy scikit-learn matplotlib \
   tqdm numba
+
+cd diff-curvature
+./setup.sh
