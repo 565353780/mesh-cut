efc890754c631b3dea7d95ac917a9a430f767030
chLi <565353780@qq.com>
2025-08-26 16:20:54 +0800

    create average mesh cutter module

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
+from mesh_cut.Demo.average_mesh_cutter import demo as demo_cut_mesh_average
 from mesh_cut.Demo.mesh_cutter import demo as demo_cut_mesh
 
 if __name__ == "__main__":
-    demo_cut_mesh()
+    demo_cut_mesh_average()
+    # demo_cut_mesh()
--- mesh_cut/Demo/average_mesh_cutter.py
+++ mesh_cut/Demo/average_mesh_cutter.py
@@ -1,16 +1,16 @@
 import sys
 
 sys.path.append("../mesh-sample")
-sys.path.append("../diff-curvature")
 
 import os
 import time
-from mesh_cut.Module.mesh_cutter import MeshCutter
+from mesh_cut.Module.average_mesh_cutter import AverageMeshCutter
 
 
 def demo():
     # 设置输入和输出路径
     mesh_file_path = "/Users/chli/chLi/Dataset/vae-eval/mesh/000.obj"
+    dist_max = 1.0 / 200
     output_dir = "./output"
 
     # 确保输出目录存在
@@ -18,27 +18,26 @@ def demo():
         os.makedirs(output_dir)
 
     # 设置分割参数
-    sub_mesh_num = 400
+    sub_mesh_num = 8192
     points_per_submesh = 1024
 
     print("Loading mesh from", mesh_file_path)
-    mesh_cutter = MeshCutter(mesh_file_path)
-
-    # 可视化原始网格的曲率
-    # print("Visualizing mesh curvature...")
-    # mesh_cutter.visualizeCurvature()
+    average_mesh_cutter = AverageMeshCutter(mesh_file_path, dist_max)
 
     # 执行网格切割
     print(f"Cutting mesh into {sub_mesh_num} segments...")
     start_time = time.time()
-    mesh_cutter.cutMesh(sub_mesh_num, points_per_submesh)
+    average_mesh_cutter.cutMesh(sub_mesh_num, points_per_submesh)
     end_time = time.time()
     print(f"Mesh cutting completed in {end_time - start_time:.2f} seconds")
 
-    print("sub mesh sample points.shape:", mesh_cutter.sub_mesh_sample_points.shape)
+    print(
+        "sub mesh sample points.shape:",
+        average_mesh_cutter.sub_mesh_sample_points.shape,
+    )
 
     print("Render face labels...")
-    mesh_cutter.renderFaceLabels()
+    average_mesh_cutter.renderFaceLabels()
 
     # print("Render sub meshes...")
     # mesh_cutter.renderSubMeshSamplePoints()
