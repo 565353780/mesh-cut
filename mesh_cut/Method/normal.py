b3d3691d9fc5a4f5a9ed103e3387667d5074e0ab
chLi <565353780@qq.com>
2025-08-27 22:33:37 +0800

    update normalize algo to support single vector

--- mesh_cut/Method/normal.py
+++ mesh_cut/Method/normal.py
@@ -2,13 +2,27 @@ import numpy as np
 
 
 def normalize(vectors: np.ndarray) -> np.ndarray:
-    norm = np.linalg.norm(vectors, axis=1)
-    normed_vectors = np.zeros_like(vectors)
+    if vectors.ndim == 1:
+        norm = np.linalg.norm(vectors)
 
-    valid_norm_idxs = np.where(norm > 0)[0]
+        if norm == 0:
+            return vectors
 
-    normed_vectors[valid_norm_idxs] = vectors[valid_norm_idxs] / norm[
-        valid_norm_idxs
-    ].reshape(-1, 1)
+        return vectors / norm
 
-    return normed_vectors
+    if vectors.ndim == 2:
+        norm = np.linalg.norm(vectors, axis=1)
+        normed_vectors = np.zeros_like(vectors)
+
+        valid_norm_idxs = np.where(norm > 0)[0]
+
+        normed_vectors[valid_norm_idxs] = vectors[valid_norm_idxs] / norm[
+            valid_norm_idxs
+        ].reshape(-1, 1)
+
+        return normed_vectors
+
+    print("[ERROR][normal::normalize]")
+    print("\t vectors dim not valid!")
+    print("\t vectors.shape:", vectors.shape)
+    return np.array([])
