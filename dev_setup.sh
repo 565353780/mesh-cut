43e07e645e51fdd67c9a887c4362c622a815bdf7
chLi <565353780@qq.com>
2026-03-27 01:02:05 +0800

    add hole fill support

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,14 +1,18 @@
 cd ..
 git clone git@github.com:565353780/mesh-sample.git
 git clone git@github.com:565353780/diff-curvature.git
+git clone git@github.com:565353780/hole-fill.git
 
-pip install -U numpy open3d scipy scikit-learn matplotlib \
-  tqdm shapely rtree mapbox-earcut trimesh
+pip install scikit-learn matplotlib shapely rtree \
+  mapbox-earcut
 
 cd mesh-sample
 ./dev_setup.sh
 
 cd ../diff-curvature
+./dev_setup.sh
+
+cd ../hole-fill
 ./dev_setup.sh
 
 cd ../mesh-cut
--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 cd ..
 git clone https://github.com/565353780/mesh-sample.git
 git clone https://github.com/565353780/diff-curvature.git
+git clone https://github.com/565353780/hole-fill.git
 
-pip install -U numpy open3d scipy scikit-learn matplotlib \
-  tqdm shapely rtree mapbox-earcut trimesh
+pip install scikit-learn matplotlib shapely rtree \
+  mapbox-earcut
 
 cd mesh-sample
 ./setup.sh
 
 cd ../diff-curvature
+./setup.sh
+
+cd ../hole-fill
 ./setup.sh
 
 cd ../mesh-cut
